2bafe6f175d3a1d6c4df4ae2328c4e605809dbb4
Gleb Dmitrievich <gdmitrievich25@gmail.com>
2025-11-12 16:32:07 +0300

    refactor: Init project with the Spring Initializer tool

---
+++ backend/src/main/java/com/aicareerroad/app/AiCareerRoadApplication.java
@@ -0,0 +1,13 @@
+package com.aicareerroad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AiCareerRoadApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(AiCareerRoadApplication.class, args);
+  }
+
+}
---
+++ backend/src/test/java/com/aicareerroad/app/AiCareerRoadApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aicareerroad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AiCareerRoadApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
